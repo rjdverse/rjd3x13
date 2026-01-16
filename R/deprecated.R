9c45140627ce439fec7e190a39634f54a8a8ca34
tbarthelemy <tanguy.barthelemy@insee.fr>
2026-01-16 16:03:36 +0000

    add title and @name tag to deprecated

--- R/deprecated.R
+++ R/deprecated.R
@@ -1,26 +1,26 @@
-#' Deprecated functions
+#' @title Deprecated functions
 #'
-#'
-
 #' @name deprecated-rjd3x13
+
+#' @rdname deprecated-rjd3x13
 #' @export
 spec_x13 <- function(name = c("rsa4", "rsa0", "rsa1", "rsa2c", "rsa3", "rsa5c")) {
     .Deprecated("x13_spec")
     x13_spec(name)
 }
-#' @name deprecated-rjd3x13
+#' @rdname deprecated-rjd3x13
 #' @export
 spec_regarima <- function(name = c("rg4", "rg0", "rg1", "rg2c", "rg3", "rg5c")) {
     .Deprecated("regarima_spec")
     regarima_spec(name)
 }
-#' @name deprecated-rjd3x13
+#' @rdname deprecated-rjd3x13
 #' @export
 spec_x11 <- function() {
     .Deprecated("x11_spec")
     x11_spec()
 }
-#' @name deprecated-rjd3x13
+#' @rdname deprecated-rjd3x13
 #' @export
 fast_x13 <- function(ts,
                      spec = c("rsa4", "rsa0", "rsa1", "rsa2c", "rsa3", "rsa5c"),
@@ -29,7 +29,7 @@ fast_x13 <- function(ts,
     .Deprecated("x13_fast")
     x13_fast(ts, spec, context, userdefined)
 }
-#' @name deprecated-rjd3x13
+#' @rdname deprecated-rjd3x13
 #' @export
 fast_regarima <- function(ts,
                           spec = c("rg4", "rg0", "rg1", "rg2c", "rg3", "rg5c"),
@@ -39,7 +39,7 @@ fast_regarima <- function(ts,
     regarima_fast(ts, spec, context, userdefined)
 }
 
-#' @name deprecated-rjd3x13
+#' @rdname deprecated-rjd3x13
 #' @export
 .jx13 <- function(ts,
                   spec = c("rg4", "rg0", "rg1", "rg2c", "rg3", "rg5c"),
@@ -49,7 +49,7 @@ fast_regarima <- function(ts,
     jx13(ts, spec, context, userdefined)
 }
 
-#' @name deprecated-rjd3x13
+#' @rdname deprecated-rjd3x13
 #' @export
 userdefined_variables_x13 <- function(x = c("X-13", "RegArima", "X-11"))  {
     .Deprecated("x13_dictionary")
